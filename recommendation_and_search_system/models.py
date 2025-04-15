0f877ace54cda93671aeec93d2284a43c927572e
Lohit20 <ashwalohit@gmail.com>
2025-04-15 15:00:31 +0100

    Adding Service which Recommendation Items

---
+++ recommendation_and_search_system/models.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+
+# Pydantic model
+class Product(BaseModel):
+    product_id: int
+    name: str
+    main_category: str
+    sub_category: str
+    image: str
+    ratings: float
+    no_of_ratings: int
+    actual_price: float
+    discount_price: float
+    stock: int
+
+
